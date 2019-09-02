e298c2d203304a66b1856062a7db49c2fdce2d9d
Thomas Bonk <thomas.bonk@sap.com>
2019-09-02 16:54:20 +0200

    changed pom by adding checkstyle stuff

--- backend/src/main/java/de/meandmymac/diabetesanalytics/backend/Application.java
+++ backend/src/main/java/de/meandmymac/diabetesanalytics/backend/Application.java
@@ -9,13 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	/**
-	 * The entry point of the application.
-	 *
-	 * @param args the input arguments
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  /**
+   * The entry point of the application.
+   *
+   * @param args the input arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
