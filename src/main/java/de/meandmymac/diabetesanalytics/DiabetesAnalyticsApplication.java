6a37623517b715221f57495b0ba90f9a0e0d3302
Thomas Bonk <thomas.bonk@sap.com>
2019-09-05 15:05:49 +0200

    added run method

--- src/main/java/de/meandmymac/diabetesanalytics/DiabetesAnalyticsApplication.java
+++ src/main/java/de/meandmymac/diabetesanalytics/DiabetesAnalyticsApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiabetesAnalyticsApplication {
+public class DiabetesAnalyticsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiabetesAnalyticsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+
+	}
 }
