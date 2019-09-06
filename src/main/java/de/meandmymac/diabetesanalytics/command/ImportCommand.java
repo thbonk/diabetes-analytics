7aad192efe64261003ff2ddf8fa6d5cc35c18a9d
Thomas Bonk <thomas.bonk@sap.com>
2019-09-06 13:53:28 +0200

    implemented some minor stuff for the import format

--- src/main/java/de/meandmymac/diabetesanalytics/command/ImportCommand.java
+++ src/main/java/de/meandmymac/diabetesanalytics/command/ImportCommand.java
@@ -1,6 +1,8 @@
 package de.meandmymac.diabetesanalytics.command;
 
 import java.util.concurrent.Callable;
+import de.meandmymac.diabetesanalytics.DiabetesAnalyticsApplication;
+import lombok.Getter;
 import picocli.CommandLine;
 
 @CommandLine.Command(
@@ -8,9 +10,25 @@ import picocli.CommandLine;
     description = "Import files with data",
     mixinStandardHelpOptions = true)
 public class ImportCommand implements Runnable {
+  @CommandLine.ParentCommand
+  private DiabetesAnalyticsApplication applicationCommand;
+
+  @CommandLine.Option(
+      names = { "-i", "--import-file" },
+      description = "Path to the MS Excel or CSV file",
+      paramLabel = "FILEPATH",
+      required = true)
+  @Getter
+  private String filepath;
+
+  @CommandLine.Option(
+      names = { "-m", "--attribute-mapping" },
+      description = "JSON file that contains the mapping between the cells and the attributes",
+      paramLabel = "ATTRIBUTE-MAPPING-FILEPATH")
+  @Getter
+  private String attributeMappingFilepath;
 
   @Override
   public void run() {
-
   }
 }
